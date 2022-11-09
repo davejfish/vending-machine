7c45eb81c9d913e2eafecce9d87de8cccbdbe3af
Dave Fisher <dave.j.fish@gmail.com>
2022-11-08 17:27:56 -0800

    multiple yen added alongside usd

---
+++ vending-machine/currencies.ts
@@ -0,0 +1,30 @@
+interface Coins {
+  [coinName: string]: number
+}
+
+  const usd: Coins = {
+    quarters: 25,
+    dimes: 10,
+    nickels: 5,
+    pennies: 1
+}
+
+  const yen: Coins = {
+  五百円玉: 500,
+  百円玉: 100,
+  五十円玉: 50,
+  十円玉: 10,
+  五円玉: 5,
+  一円玉: 1
+}
+const coins = {usd, yen}
+
+export default coins;
+
+
+// const coins: { [usd: string]: { 
+//   quarters: number,
+//   dimes: number,
+//   nickels: number,
+//   pennies: number
+// }} = {
--- vending-machine/vending-machine.ts
+++ vending-machine/vending-machine.ts
@@ -1,8 +1,9 @@
 import process from 'node:process'
 
-let arg = null
-let cost = null
-let payment = null
+let arg = null;
+let cost = null;
+let payment = null;
+let tender = 'usd';
 
 while((arg = process.argv.shift()) != null) {
   if(arg == '--item-cost') {
@@ -11,6 +12,10 @@ while((arg = process.argv.shift()) != null) {
   } else if(arg == '--payment') {
     const paymentInput = process.argv.shift()
     payment = Math.floor(Number(paymentInput || '0') * 100)
+  } 
+  else if (arg == '--tender') {
+    const tenderInput = process.argv.shift();
+    tender = tenderInput || 'usd';
   }
 }
 
@@ -26,13 +31,22 @@ if(payment == null) {
   console.error('--payment is required but not provided. Exiting.')
   process.exit(2)
 }
-// Implement your vending machine here!
+if (tender == null) {
+  console.error('--tender is required to be a string. Exiting')
+  process.exit(3);
+}
 
-const coins: object = {
-  quarters: 25,
-  dimes: 10,
-  nickels: 5,
-  pennies: 1
+// Implement your vending machine here!
+import coins from './currencies.js';
+
+let currency = coins.usd;
+switch(tender) {
+  case 'usd': 
+    currency = coins.usd
+    break;
+  case 'yen':
+    currency = coins.yen;
+    break;
 }
 
 let changeToBeReturned = payment - cost;
@@ -40,16 +54,16 @@ let changeObject: Record<string, number> = {};
 
 if (changeToBeReturned < 0) {
   console.log('status: failure \nmessage: Not enough money')
-  process.exit(3);
+  process.exit(4);
 }
 
-while (changeToBeReturned > 0) {
-  for (let [coin, value] of Object.entries(coins)) {
+
+  for (let [coin, value] of Object.entries(currency)) {
     while (changeToBeReturned >= value) {
       changeObject[coin] = Math.floor(changeToBeReturned / value);
       changeToBeReturned = changeToBeReturned % value;
     }
   }
-}
+
 
 console.log('status: success\nyour change is: ', changeObject);
